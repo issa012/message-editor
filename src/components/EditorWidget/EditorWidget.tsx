cc3c3d70da6af4afb08aa9dcd066175a852e9f39
Yeldar Issa <yeldar.issa@gmail.com>
2023-07-07 16:25:18 +0600

    delete, add variable functionality

--- src/components/EditorWidget/EditorWidget.tsx
+++ src/components/EditorWidget/EditorWidget.tsx
@@ -5,28 +5,18 @@ import styles from './EditerWidget.module.css';
 import { useRef, useState } from 'react';
 import InputTree from './InputTree/InputTree';
 
-const variables = ['firstname', 'lastname', 'company', 'position'];
+const createTemplate: (node: HTMLDivElement) => string[] = (node) => {
+  console.log(node);
+  let arr: string[] = [];
+  arr.push((node.children[0] as HTMLTextAreaElement).value);
 
-const tree = {
-  ifCond: '{company}',
-  ifTrue: 'I know you work at {company}',
-  child: {
-    ifCond: '{position}',
-    ifTrue: 'as {position}',
-    child: {
-      ifCond: '',
-      ifTrue: '',
-      child: {},
-      ifFalse: '',
-    },
-    ifFalse: '',
-  },
-  ifFalse: '',
+  arr.push((node.children[2] as HTMLTextAreaElement).value);
+
+  return [];
 };
 
 const EditorWidget = ({
   arrVarNames,
-  template,
   callbackSave,
 }: {
   arrVarNames: string[];
@@ -34,9 +24,14 @@ const EditorWidget = ({
   callbackSave: () => Promise<void>;
 }) => {
   const [lastElement, setLastElement] = useState<HTMLTextAreaElement | null>(null);
-  const [depth, setDepth] = useState(1);
-  const treeRef = useRef(null);
-  console.log(treeRef);
+  const [depth, setDepth] = useState(2);
+  const [height] = useState(0);
+  const treeRef = useRef<HTMLDivElement>(null);
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  if (treeRef.current) {
+    // createTemplate(treeRef.current);
+  }
 
   const handleAddVariable = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (lastElement) {
@@ -46,6 +41,7 @@ const EditorWidget = ({
         prevValue.slice(0, startPos) +
         e.currentTarget.value +
         prevValue.slice(startPos, lastElement.value.length);
+
       lastElement.focus();
       lastElement.selectionStart = startPos + e.currentTarget.value.length;
       lastElement.selectionEnd = startPos + e.currentTarget.value.length;
@@ -60,26 +56,41 @@ const EditorWidget = ({
     <div className={styles.container}>
       <h1 className={styles.title}>Message template editor</h1>
       <div className={styles.buttonGroup}>
-        {variables.map((variable) => {
+        {arrVarNames.map((varName) => {
           return (
             <Button
               onClick={handleAddVariable}
-              key={variable}
-              value={`{${variable}}`}
-            >{`{${variable}}`}</Button>
+              key={varName}
+              value={`{${varName}}`}
+            >{`{${varName}}`}</Button>
           );
         })}
       </div>
       <Button onClick={() => setDepth((prev) => ++prev)}>[IF-THEN-ELSE]</Button>
 
       <div ref={treeRef} id="tree">
-        <Textarea onFocus={handleFocus} />
+        <Textarea
+          onFocus={handleFocus}
+          value={topText}
+          onChange={(e) => setTopText(e.target.value)}
+        />
         <InputTree
           handleFocus={handleFocus}
           depth={depth}
-          onDelete={() => setDepth((prev) => --prev)}
-        ></InputTree>
-        {depth > 0 ? <Textarea onFocus={handleFocus} /> : null}
+          height={height}
+          setDepth={setDepth}
+          setText={() => {
+            setTopText(topText.concat(bottomText));
+            setBottomText('');
+          }}
+        />
+        {depth > 0 ? (
+          <Textarea
+            onFocus={handleFocus}
+            value={bottomText}
+            onChange={(e) => setBottomText(e.target.value)}
+          />
+        ) : null}
       </div>
     </div>
   );
--- src/components/EditorWidget/InputTree/InputTree.tsx
+++ src/components/EditorWidget/InputTree/InputTree.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Button } from '../../ui/button/button';
 import { Textarea } from '../../ui/textarea/textarea';
 import styles from './InputTree.module.css';
@@ -5,13 +6,29 @@ import styles from './InputTree.module.css';
 type InputTreeProps = {
   depth: number;
   className?: string;
-  onDelete: () => void;
+  setDepth: React.Dispatch<React.SetStateAction<number>>;
   handleFocus: (e: React.FocusEvent<HTMLTextAreaElement>) => void;
+  setText: () => void;
+  height: number;
 };
 
-const InputTree = ({ depth, className, onDelete, handleFocus }: InputTreeProps) => {
-  if (depth == 0) return null;
+const InputTree = ({
+  depth,
+  className,
+  setDepth,
+  handleFocus,
+  height,
+  setText,
+}: InputTreeProps) => {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
 
+  const onDelete = () => {
+    setDepth(height);
+    setText();
+  };
+
+  if (depth === 0) return null;
   return (
     <div className={className}>
       <div className={styles.container}>
@@ -25,18 +42,31 @@ const InputTree = ({ depth, className, onDelete, handleFocus }: InputTreeProps) 
         <div className={styles.left}>
           <span>THEN</span>
         </div>
-        <Textarea onFocus={handleFocus} />
+        <Textarea
+          value={topText}
+          onChange={(e) => setTopText(e.target.value)}
+          onFocus={handleFocus}
+        />
       </div>
       <InputTree
         handleFocus={handleFocus}
         className={styles.innerTree}
+        height={height + 1}
         depth={depth - 1}
-        onDelete={onDelete}
+        setDepth={setDepth}
+        setText={() => {
+          setTopText(topText.concat(bottomText));
+          setBottomText('');
+        }}
       ></InputTree>
       {depth - 1 > 0 ? (
         <div className={styles.container}>
           <div className={styles.left}></div>
-          <Textarea onFocus={handleFocus} />
+          <Textarea
+            value={bottomText}
+            onChange={(e) => setBottomText(e.target.value)}
+            onFocus={handleFocus}
+          />
         </div>
       ) : null}
       <div className={styles.container}>
