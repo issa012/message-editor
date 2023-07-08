a3cd43dbee29694aeb5287b4ce27025c3f0dd337
Yeldar Issa <yeldar.issa@gmail.com>
2023-07-09 05:21:44 +0600

    changed names of vars for readability

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,14 @@
 //@ts-nocheck
 const helper = (node, depth) => {
   let result = {};
-  result.IF = node.children[0].lastChild.value.slice(1, -1);
-  result.THEN = node.children[1].lastChild.value;
+  result.condition = node.children[0].lastChild.value.slice(1, -1);
+  result.conditionTrue = node.children[1].lastChild.value;
   if (depth === 1) {
-    result.ELSE = node.children[2].lastChild.value;
+    result.conditionFalse = node.children[2].lastChild.value;
   } else {
     result.child = helper(node.children[2], depth - 1);
-    result.BOTTOM = node.children[3].lastChild.value;
-    result.ELSE = node.children[4].lastChild.value;
+    result.closing = node.children[3].lastChild.value;
+    result.conditionFalse = node.children[4].lastChild.value;
   }
   return result;
 };
@@ -18,34 +18,34 @@ export const generateTemplate: (node: HTMLDivElement, depth: number) => string[]
   depth
 ) => {
   let template = {};
-  template.TOP = node.children[0].value;
+  template.intro = node.children[0].value;
   if (depth === 0) {
     return template;
   }
   template.child = helper(node.children[1], depth);
-  template.BOTTOM = node.children[2].value;
+  template.closing = node.children[2].value;
 
   return template;
 };
 
 export const generateMessage = (template, values) => {
   let result: string[] = [];
-  result.push(replacer(template.TOP, values));
+  result.push(replacer(template.intro, values));
 
-  if (!template.child) return replacer(template.TOP, values);
+  if (!template.child) return replacer(template.intro, values);
 
   generateMessageHelper(template.child, values, result);
 
-  result.push(replacer(template.BOTTOM, values));
+  result.push(replacer(template.closing, values));
   return result.join('');
 };
 
 function generateMessageHelper(node, values, result) {
-  if (!values[node.IF]) {
-    result.push(node.ELSE);
+  if (!values[node.condition]) {
+    result.push(node.conditionFalse);
     return;
   } else {
-    result.push(node.THEN.replaceAll(`{${node.IF}}`, values[node.IF]));
+    result.push(node.conditionTrue.replaceAll(`{${node.condition}}`, values[node.condition]));
   }
 
   if (!node.child) {
@@ -54,7 +54,7 @@ function generateMessageHelper(node, values, result) {
 
   generateMessageHelper(node.child, values, result);
 
-  result.push(node.BOTTOM);
+  result.push(node.closing);
 }
 
 const replacer = (initialString: string, values) => {
