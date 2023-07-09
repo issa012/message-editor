01b28856a86ab36266d288addd99e0ee3fbbb452
Yeldar Issa <yeldar.issa@gmail.com>
2023-07-10 02:13:35 +0600

    added label

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import styles from './input.module.css';
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className = '', type, ...props }, ref) => {
-    return <input type={type} className={`${styles.input} ${className}`} ref={ref} {...props} />;
+  ({ className = '', type, label, ...props }, ref) => {
+    return (
+      <div className={styles.container}>
+        <label>{label}</label>
+        <input type={type} className={`${styles.input} ${className}`} ref={ref} {...props} />
+      </div>
+    );
   }
 );
 Input.displayName = 'Input';
