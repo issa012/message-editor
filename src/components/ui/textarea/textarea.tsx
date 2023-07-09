c496ed7a554e24b0a5b8c877e8e4350a25958712
Yeldar Issa <yeldar.issa@gmail.com>
2023-07-10 02:14:13 +0600

    style

--- src/components/ui/textarea/textarea.tsx
+++ src/components/ui/textarea/textarea.tsx
@@ -4,15 +4,8 @@ import TextareaAutosize, { TextareaAutosizeProps } from 'react-textarea-autosize
 import styles from './textarea.module.css';
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaAutosizeProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <TextareaAutosize
-        className={`${styles.textarea} ${className}`}
-        ref={ref}
-        {...props}
-        minRows={2}
-      />
-    );
+  ({ ...props }, ref) => {
+    return <TextareaAutosize className={`${styles.textarea}`} ref={ref} {...props} minRows={2} />;
   }
 );
 Textarea.displayName = 'Textarea';
