2c4aab4703335bdc1eebdb401abd05b9e7750eb2
Yeldar Issa <yeldar.issa@gmail.com>
2023-07-12 16:33:28 +0600

    delete values, change values implemented

--- src/components/EditorWidget/EditorWidget.tsx
+++ src/components/EditorWidget/EditorWidget.tsx
@@ -1,11 +1,24 @@
 import { useState } from 'react';
 import { Textarea } from '../ui/textarea/textarea';
 import InputTree from './InputTree/InputTree';
-const templateObj = {
+
+export type Template = {
+  condition: string;
+  condTrue: { value: string; children: Array<Template> };
+  condFalse: { value: string; children: Array<Template> };
+  additional: { value: string; children: Array<Template> };
+};
+
+export type MyTemplate = {
+  main: string;
+  children: Template[];
+};
+
+const templateObj: MyTemplate = {
   main: '1',
   children: [
     {
-      condition: '1',
+      condition: '2',
       condTrue: { value: '2', children: [] },
       condFalse: {
         value: '2',
@@ -38,14 +51,68 @@ const EditorWidget = ({
   template?: string;
   callbackSave: () => Promise<void>;
 }) => {
-  const [values, setValues] = useState(templateObj);
+  const [values, setValues] = useState<MyTemplate>(templateObj);
+  const [lastElement, setLastElement] = useState('children');
+
+  function handleValuesChange(path: string, e: React.ChangeEvent<HTMLTextAreaElement>) {
+    const pathId = path.split('.');
+    const copy = JSON.parse(JSON.stringify(values));
+    let curr = copy;
+    for (let i = 0; i < pathId.length; i++) {
+      curr = curr[pathId[i]];
+    }
+    if (e.target.name === 'condition') {
+      curr.condition = e.target.value;
+    } else {
+      curr[e.target.name].value = e.target.value;
+    }
+    setValues(copy);
+  }
+
+  function handleDelete(path: string, e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    const pathId = path.split('.');
+    const copy = JSON.parse(JSON.stringify(values));
+    let curr = copy;
+    for (let i = 0; i < pathId.length - 1; i++) {
+      curr = curr[pathId[i]];
+    }
+    curr.splice(pathId.at(-1), 1);
+    setValues(copy);
+  }
+
+  function handleFocus(path: string, e: React.ChangeEvent<HTMLTextAreaElement>) {
+    if (e.target.name) {
+      setLastElement(path.concat('.', e.target.name));
+    } else {
+      setLastElement(path);
+    }
+  }
+
+  function handleAddElement(path: string) {
+    const copy = JSON.parse(JSON.stringify(values));
+
+    setValues(copy);
+  }
 
   return (
     <div>
       <div>
         <h2>Message Template Editor</h2>
-        <Textarea value={values.main} />
-        <InputTree children={values.children} />
+        <div></div>
+        <Textarea
+          value={values.main}
+          onChange={(e) => setValues({ ...values, main: e.target.value })}
+          onFocus={(e) => handleFocus('children', e)}
+        />
+        <InputTree
+          children={values.children}
+          values={values}
+          setValues={setValues}
+          path=""
+          handleValuesChange={handleValuesChange}
+          handleDelete={handleDelete}
+          handleFocus={handleFocus}
+        />
       </div>
     </div>
   );
--- src/components/EditorWidget/InputTree/InputTree.tsx
+++ src/components/EditorWidget/InputTree/InputTree.tsx
@@ -1,47 +1,101 @@
-import { useRef } from 'react';
 import { Button } from '../../ui/button/button';
 import { Textarea } from '../../ui/textarea/textarea';
 import styles from './InputTree.module.css';
+import { MyTemplate, Template } from '../EditorWidget';
 
 type InputTreeProps = {
-  children: any;
+  children: Template[];
+  values: MyTemplate;
+  setValues: React.Dispatch<React.SetStateAction<MyTemplate>>;
+  handleChange?: (id?: number, values?: any) => void;
+  path: string;
+  handleValuesChange: (path: string, e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  handleDelete: (path: string, e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  handleFocus: (path: string, e: React.FocusEvent<HTMLTextAreaElement, Element>) => void;
 };
 
-const InputTree = ({ children }: InputTreeProps) => {
-  if (!children) return null;
+const InputTree = ({
+  children,
+  values,
+  setValues,
+  path,
+  handleValuesChange,
+  handleDelete,
+  handleFocus,
+}: InputTreeProps) => {
+  if (!children.length) return null;
+  path = path.concat('children');
   return (
     <>
-      {children.map((node: any) => (
+      {children.map((node: Template, id: number) => (
         <div className={styles.container}>
-          <div>
-            <div className={styles.textareaContainer}>
-              <span className={styles.chip}>
-                <Button>Delete</Button>
-                IF
-              </span>
-              <Textarea value={node.condition} />
-            </div>
+          <div className={styles.textareaContainer}>
+            <span className={styles.chip}>
+              <Button onClick={(e) => handleDelete(path.concat('.', String(id)), e)}>Delete</Button>
+              IF
+            </span>
+            <Textarea
+              value={node.condition}
+              name="condition"
+              onChange={(e) => handleValuesChange(path.concat('.', String(id)), e)}
+              onFocus={(e) => handleFocus(path.concat('.', String(id)), e)}
+            />
           </div>
-          <div>
-            <div className={styles.textareaContainer}>
-              <span className={styles.chip}>THEN</span>
-              <Textarea value={node.condTrue.value} />
-            </div>
-            <InputTree children={node.condTrue.children} />
+          <div className={styles.textareaContainer}>
+            <span className={styles.chip}>THEN</span>
+            <Textarea
+              value={node.condTrue.value}
+              name="condTrue"
+              onChange={(e) => handleValuesChange(path.concat('.', String(id)), e)}
+              onFocus={(e) => handleFocus(path.concat('.', String(id)), e)}
+            />
           </div>
-          <div>
-            <div className={styles.textareaContainer}>
-              <span className={styles.chip}>ELSE</span>
-              <Textarea value={node.condFalse.value} />
-            </div>
-            <InputTree children={node.condFalse.children} />
+          <InputTree
+            children={node.condTrue.children}
+            values={values}
+            setValues={setValues}
+            path={path.concat('.', String(id), '.condTrue.')}
+            handleValuesChange={handleValuesChange}
+            handleDelete={handleDelete}
+            handleFocus={handleFocus}
+          />
+          <div className={styles.textareaContainer}>
+            <span className={styles.chip}>ELSE</span>
+            <Textarea
+              value={node.condFalse.value}
+              name="condFalse"
+              onChange={(e) => handleValuesChange(path.concat('.', String(id)), e)}
+              onFocus={(e) => handleFocus(path.concat('.', String(id)), e)}
+            />
           </div>
+          <InputTree
+            handleValuesChange={handleValuesChange}
+            children={node.condFalse.children}
+            values={values}
+            setValues={setValues}
+            path={path.concat('.', String(id), '.condFalse.')}
+            handleDelete={handleDelete}
+            handleFocus={handleFocus}
+          />
           <div className={styles.additional}>
             <div className={styles.textareaContainer}>
               <span className={styles.chip}></span>
-              <Textarea value={node.additional.value} />
+              <Textarea
+                value={node.additional.value}
+                name="additional"
+                onChange={(e) => handleValuesChange(path.concat('.', String(id)), e)}
+                onFocus={(e) => handleFocus(path.concat('.', String(id)), e)}
+              />
             </div>
-            <InputTree children={node.additional.children} />
+            <InputTree
+              handleValuesChange={handleValuesChange}
+              children={node.additional.children}
+              values={values}
+              setValues={setValues}
+              path={path.concat('.', String(id), '.additional.')}
+              handleDelete={handleDelete}
+              handleFocus={handleFocus}
+            />
           </div>
         </div>
       ))}
